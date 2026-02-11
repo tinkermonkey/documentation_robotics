f9e3353abdae55b55fdf3f4ba43340f911b871e7
Tinker Monkey <1573324+tinkermonkey@users.noreply.github.com>
2026-02-11 09:51:15 -0500

    Fixed CORS issue with neo4j browser

--- spec/neo4j/cors-server.py
+++ spec/neo4j/cors-server.py
@@ -9,7 +9,7 @@ class CORSRequestHandler(SimpleHTTPRequestHandler):
     def end_headers(self):
         self.send_header('Access-Control-Allow-Origin', '*')
         self.send_header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS')
-        self.send_header('Access-Control-Allow-Headers', 'Content-Type, Accept, X-Requested-With')
+        self.send_header('Access-Control-Allow-Headers', '*')
         self.send_header('Access-Control-Max-Age', '86400')
         super().end_headers()
 
